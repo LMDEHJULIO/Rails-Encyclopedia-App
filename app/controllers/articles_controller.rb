eca8c7febf0ae26200638010db7e002fe1822d3f
Julio Rodriguez <Julio@Julios-MacBook-Pro.local>
2015-12-11 20:07:17 -0500

    Categories added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,20 @@
 class ArticlesController < ApplicationController
 	before_action :find_article, only: [:show, :edit, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 	def index
-		@articles = Article.all.order("created_at DESC")
+		if params[:category].blank?
+			@articles = Article.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@articles = Article.where(category_id: @category_id).order("created_at DESC")
+		end
 	end 
 	def new
-		@article = Article.new
+		@article = current_user.articles.build
 	end
 
 	def create
-		@article = Article.new(article_params)
+		@article = current_user.articles.build(article_params)
 		if @article.save 
 			redirect_to @article 
 		else 
@@ -23,7 +29,7 @@ class ArticlesController < ApplicationController
 	private
 
 	def article_params 
-		params.require(:article).permit(:title, :content)
+		params.require(:article).permit(:title, :content, :category_id)
 	end
 	def find_article
 		@article = Article.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211231639) do
+ActiveRecord::Schema.define(version: 20151212003437) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "category_id"
+  end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
